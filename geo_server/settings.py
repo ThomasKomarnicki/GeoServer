7bd4863bd3e1eff70fbeaa71de545e03ae4e7721
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2015-09-27 16:18:24 -0400

    remove cling for static files

--- geo_server/settings.py
+++ geo_server/settings.py
@@ -15,7 +15,6 @@ import os
 # import confidential
 import dj_database_url
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
@@ -23,6 +22,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
